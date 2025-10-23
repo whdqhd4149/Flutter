bbfeaf0776072d709bfed3712193d770786b85db
whdqhd4149 <whdqhd4149@gmail.com>
2025-10-23 12:30:03 +0900

    10-23오전

---
+++ ch01/01_hello.dart
@@ -0,0 +1,19 @@
+/*
+  날짜: 2025/10/23
+  이름: 이종봉
+  내용: 1장 Flutter 개발환경 구축
+
+  - Android Studio 설치
+  - Flutter SDK 다운로드 및 압축 해제
+  - Android Studio > plugins > flutter, dart 플러그인 설치
+ */
+
+import 'dart:io';
+
+void main(){
+  print('Hello Wold');
+  print("Hello Dart!");
+  print("Dart Version : ${Platform.version}");
+
+}
+
