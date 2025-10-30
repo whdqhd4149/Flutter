aca44151fed80915b2dfbd0c7caba1e0772918be
whdqhd4149 <whdqhd4149@gmail.com>
2025-10-30 09:03:15 +0900

    10-29 오후

---
+++ ch07/lib/user1/user1_list.dart
@@ -0,0 +1,84 @@
+
+import 'package:ch07/user1/user1.dart';
+import 'package:ch07/user1/user1_register.dart';
+import 'package:ch07/user1/user1_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class User1List extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _User1ListState();
+}
+
+class _User1ListState extends State<User1List> {
+
+  final service = User1Service();
+
+  // late : 초기화를 연기하고 선언
+  late Future<List<User1>> futureUserList;
+
+  @override
+  void initState() {
+
+    // 목록 데이터 요청하기
+    futureUserList = service.getUsers();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('User1 목록'),),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            // 등록 이동
+            Navigator.push(context, MaterialPageRoute(
+                builder: (context) => User1Register()
+            ));
+          },
+        child: const Icon(Icons.add),
+      ),
+      body: FutureBuilder(
+          future: futureUserList,
+          builder: (context, snapshot){
+
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const CircularProgressIndicator();
+            }else if(snapshot.hasError) {
+              return Text('에러발생 : ${snapshot.error}');
+            }
+
+            final userList = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: userList.length,
+              itemBuilder: (context, index) {
+
+                final user = userList[index];
+
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      child: Text(user.name[0]),
+                    ),
+                    title: Text('${user.name}(${user.userid})'),
+                    subtitle: Text('${user.age}세(${user.birth})'),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(onPressed: (){}, icon: Icon(Icons.edit), color: Colors.blue,),
+                        IconButton(onPressed: (){}, icon: Icon(Icons.delete), color: Colors.red,),
+                      ],
+                    ),
+                  ),
+                );
+
+              }
+            );
+          }
+      ),
+    );
+  }
+  
+}
---
+++ ch07/lib/user1/user1_register.dart
@@ -0,0 +1,151 @@
+
+import 'dart:developer';
+
+import 'package:ch07/user1/user1.dart';
+import 'package:ch07/user1/user1_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class User1Register extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _User1RegisterState();
+}
+
+class _User1RegisterState extends State<User1Register> {
+
+  final _formKey = GlobalKey<FormState>();
+  final _useridController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _birthController = TextEditingController();
+  final _ageController = TextEditingController();
+
+  final service = User1Service();
+
+  String message = '';
+
+  Future<void> _submitForm() async {
+
+    if(!_formKey.currentState!.validate()) return;
+    
+    User1 inputUser = User1(
+        userid: _useridController.text,
+        name: _nameController.text,
+        birth: _birthController.text,
+        age: int.tryParse(_ageController.text) ?? 0
+    );
+
+    try {
+      User1 savedUser = await service.postUser(inputUser);
+      //print('savedUser : ${savedUser} 등록');
+      await _showDialog('등록성공', '사용자가 성공적으로 등록되었습니다.');
+
+      // 목록 이동
+      Navigator.pop(context);
+
+    }catch(err){
+      setState(() {
+        message = '등록실패, 에러발생 했습니다. $err}';
+      });
+    }
+  }
+
+  Future<void> _showDialog(String title, String message){
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: [
+          TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+
+  }
+
+
+
+  Future<void> selectBirth() async {
+    DateTime now = DateTime.now();
+    final DateTime? selectedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: DateTime(1900),
+        lastDate: now
+    );
+
+    if(selectedDate != null){
+      setState(() {
+
+        //String fmtDate = DateFormat('yyyy-MM-dd').format(selectedDate);
+        _birthController.text = selectedDate.toString();
+
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('User1 등록'),),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _useridController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '아이디 입력'
+                ),
+              ),
+              const SizedBox(height: 10,),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '이름 입력'
+                ),
+              ),
+              const SizedBox(height: 10,),
+              TextFormField(
+                controller: _birthController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '생년월일 입력'
+                ),
+                readOnly: true,
+                onTap: selectBirth,
+              ),
+              const SizedBox(height: 10,),
+              TextFormField(
+                controller: _ageController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '나이 입력'
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(onPressed: (){
+                    Navigator.pop(context);
+                  }, child: const Text('취소')),
+                  ElevatedButton(onPressed: _submitForm,
+                      child: const Text('등록')),
+                ],
+              ),
+              Text(message)
+            ],
+          )
+        ),
+      )
+    );
+  }
+}
