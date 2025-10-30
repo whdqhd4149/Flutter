fe0247a300389f659328d14c5363f4f3137bcc05
whdqhd4149 <whdqhd4149@gmail.com>
2025-10-30 12:34:39 +0900

    kmarket

---
+++ kmarket_shopping_app/android/app/src/main/kotlin/com/example/kmarket_shopping_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kmarket_shopping_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ kmarket_shopping_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:kmarket_shopping_app/screens/main/main_tab_screen.dart';
+import 'package:kmarket_shopping_app/screens/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen()
+      //home: MainTabScreen(),
+    );
+  }
+}
